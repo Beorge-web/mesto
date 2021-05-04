ba7554758403f764dd6b6470d4172097f876d3ec
Ilya Pyankov <beorgerer64@gmail.com>
2021-05-04 18:34:43 +0500

    second

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@ const addButton = document.querySelector(".profile__add-button");
 const newCard = document.querySelector("#new-card");
 const newCardClose = newCard.querySelector(".popup__close-icon");
 const createButton = newCard.querySelector(".popup__submit-button");
-const popup = document.querySelector("#edit-profile");
-const form = popup.querySelector(".popup__form");
+const editPopup = document.querySelector("#edit-profile");
+const editForm = editPopup.querySelector(".popup__form");
 const addForm = newCard.querySelector(".popup__form");
 const profileName = document.querySelector(".profile__title");
 const profileActivity = document.querySelector(".profile__subtitle");
 const saveButton = document.querySelector(".popup__submit-button");
 const editButton = document.querySelector(".profile__button");
-const closeButton = popup.querySelector(".popup__close-icon");
-const inputActivity = popup.querySelector(".popup__text_type_activity");
+const closeButton = editPopup.querySelector(".popup__close-icon");
+const inputActivity = editPopup.querySelector(".popup__text_type_activity");
 const nameInput = document.querySelector(".popup__text_type_name");
 const activityInput = document.querySelector(".popup__text_type_activity");
 const imgButton = document.querySelectorAll(".element__image");
@@ -20,7 +20,7 @@ const like = document.querySelectorAll(".element__like");
 const imgClose = document.querySelectorAll(".element__close-icon");
 const deleteButton = document.querySelectorAll(".element__delete");
 const cards = document.querySelectorAll(".element");
-const cardPopup = document.querySelectorAll(".element__photo-popup");
+const cardPopup = document.querySelector("#element__photo-popup");
 const initialCards = [
   {
     name: "Архыз",
@@ -47,30 +47,20 @@ const initialCards = [
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
   },
 ];
-function getCardElement(element) {
+function createCard(element) {
   const cardElement = tempCard.querySelector(".element").cloneNode(true);
-  const openedPopup = document.querySelector(".element__photo-popup");
-  const cardPhoto = document.querySelector(".element__photo");
-  const cardDesc = document.querySelector(".element__description");
+  const openedPopup = document.querySelector("#element__photo-popup");
+  const cardPhoto = openedPopup.querySelector(".popup__photo");
+  const cardDesc = openedPopup.querySelector(".popup__description");
   const elTitle = cardElement.querySelector(".element__title");
   const elImg = cardElement.querySelector(".element__image");
-  const place = newCard.querySelector(".popup__text_type_place");
-  const imgLink = newCard.querySelector(".popup__text_type_link");
   elTitle.textContent = element.name;
-  elImg.style.backgroundImage = "url(" + element.link + ")";
-  let imgSrc = elImg.style.backgroundImage.slice(5, -2);
-  if (element == addForm) {
-    elTitle.textContent = place.value;
-    elImg.style.backgroundImage = "url(" + imgLink.value + ")";
-    imgSrc = elImg.style.backgroundImage.slice(5, -2);
-  }
+  elImg.style.backgroundImage = `url(${element.link})`;
+  cardPhoto.alt = elTitle.textContent;
   elImg.addEventListener("click", function () {
     showPopup(openedPopup);
-    cardPhoto.src = imgSrc;
+    cardPhoto.src = elImg.style.backgroundImage.slice(5, -2);
     cardDesc.textContent = elTitle.textContent;
-  });
-  openedPopup.querySelector(".element__close-icon").addEventListener("click", function () {
-    closePopup(openedPopup);
   });
   cardElement.querySelector(".element__delete").addEventListener("click", function () {
     cardElement.remove();
@@ -78,34 +68,36 @@ function getCardElement(element) {
   cardElement.querySelector(".element__like").addEventListener("click", function () {
     likeButton(cardElement.querySelector(".element__like"));
   });
-  cardsContainer.prepend(cardElement);
-  cardsContainer.prepend(openedPopup);
-  addForm.reset();
-  closePopup(newCard);
-  return false;
-}
+  addCard(cardsContainer,cardElement);
 
+  return cardElement;
+}
+initialCards.forEach(createCard)
+
+function addCard(container, cardElement) {
+  container.prepend(cardElement);
+}
 function likeButton(item) {
   item.classList.toggle("element__like_active");
 }
 function changeProfile(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
-  profileActivity.textContent =activityInput.value;
-  closePopup(popup);
+  profileActivity.textContent = activityInput.value;
+  closePopup(editPopup);
 }
 function showPopup(item) {
   item.classList.add("popup_opened");
-  if (item == popup) {
-    popup.classList.add("popup_opened");
-    nameInput.value = profileName.textContent;
-    activityInput.value = profileActivity.textContent;
-  }
 }
 function closePopup(item) {
   item.classList.remove("popup_opened");
 }
-initialCards.forEach(getCardElement);
+function getCard(element) {
+  let name  = element.querySelector(".popup__text_type_place").value;
+  let link  = element.querySelector(".popup__text_type_link").value;
+  return {name, link};
+}
+
 newCardClose.addEventListener("click", function () {
   closePopup(newCard);
 });
@@ -113,13 +105,20 @@ addButton.addEventListener("click", function () {
   showPopup(newCard);
 });
 editButton.addEventListener("click", function () {
-  showPopup(popup);
+  showPopup(editPopup);
+  nameInput.value = profileName.textContent;
+  activityInput.value = profileActivity.textContent;
 });
-form.addEventListener("submit", changeProfile);
+editForm.addEventListener("submit", changeProfile);
 closeButton.addEventListener("click", function () {
-  closePopup(popup);
+  closePopup(editPopup);
 });
 addForm.addEventListener("submit", function (evt) {
   evt.preventDefault();
-  getCardElement(addForm);
+  createCard(getCard(addForm)); 
+  addForm.reset();
+  closePopup(newCard);
+});
+cardPopup.querySelector(".popup__close-icon").addEventListener("click", function () {
+  closePopup(cardPopup);
 });
